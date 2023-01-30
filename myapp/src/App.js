dae85ef78aba51d5d7ad999ecf4a5b92df2d71b9
Manivannan S <104342804+Manivannan-SenthamaraiKannan@users.noreply.github.com>
2023-01-30 20:45:00 +0530

    Pages Component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Login from './Components/SideBar/Pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+     
     </div>
   );
 }
---
+++ myapp/src/Components/Pages/Blank/Blank.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../App.css';
+
+const Blank = () => {
+    return (
+        <div id='page-top'>
+            
+            
+        </div>
+
+    );
+};
+
+export default Blank;
